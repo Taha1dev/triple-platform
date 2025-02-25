fc2ad567e32cf02960e431b02911b0ce3d3e3516
taha <taha@dev>
2025-02-25 19:56:47 +0300

    header home show search

--- src/features/dashboard/layout/index.tsx
+++ src/features/dashboard/layout/index.tsx
@@ -1,8 +1,17 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Outlet } from 'react-router-dom'
 import Sidebar from './Side'
 import Header from './Header'
+import { useDispatch } from 'react-redux'
+import { AppDispatch } from '@/store/store'
+import { useEffect } from 'react'
+import { showHeader } from '@/store/slices/DOMSlice'
 
 export default function PanelLayout() {
+  const dispatch = useDispatch<AppDispatch>()
+  useEffect(() => {
+    dispatch(showHeader())
+  }, [])
   return (
     <div className='flex h-screen'>
       <Sidebar />
